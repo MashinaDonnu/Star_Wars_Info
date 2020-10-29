a702096f95afc478738ec25506b60cde9a65f2f7
DImka <MashinaDonnu>
2020-10-29 22:54:54 +0200

    added custom useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,22 @@
+import {useCallback, useState} from "react"
+import axios from 'axios'
+
+export const useFetch = () => {
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const doFetch = useCallback(async (url, method, body = null) => {
+        setLoading(true)
+        try {
+            const response = await axios({url, method, body})
+            setLoading(false)
+            console.log('DDD', response)
+            return response.data
+        } catch (e) {
+            setError(e)
+            setLoading(false)
+        }
+    })
+
+    return {loading, error, doFetch}
+}
--- src/pages/Planets/Planets.js
+++ src/pages/Planets/Planets.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import {PlanetCard} from "../../components/PlanetCard/PlanetCard"
+import {useFetch} from "../../hooks/useFetch";
 
 export const Planets = () => {
+    const {loading, error, doFetch} = useFetch()
+    const [planets, setPlanets] = useState([])
+
+    const request = useCallback(async () => {
+        const data = await doFetch(process.env.REACT_APP_PLANETS_URL)
+        setPlanets(data.results)
+    }, [setPlanets])
+
+    useEffect(async () => {
+        request()
+    }, [])
+    console.log('Planets:', planets)
     return (
         <div className="planets-page">
             <div className="page-header">
